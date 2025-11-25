dbb8ad4ed3e42b9d38c6a29a93cee10d9d14d2a3
deasisnatalia <deasisnatalia@gmail.com>
2025-11-25 07:37:38 -0300

    intento13

--- mercado/mercado/wsgi.py
+++ mercado/mercado/wsgi.py
@@ -1,16 +1,6 @@
 import os
-from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mercado.settings')
 
 application = get_wsgi_application()
-
-User = get_user_model()
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="Admin123!"
-    )
