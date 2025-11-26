5dcbd2e81ed5f7b3e75ed807c58f5e269c987d81
deasisnatalia <deasisnatalia@gmail.com>
2025-11-26 05:32:39 -0300

    arreglos8

--- mercado/mercado/settings.py
+++ mercado/mercado/settings.py
@@ -149,6 +149,12 @@ CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
 CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
 CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
 
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET
+)
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
