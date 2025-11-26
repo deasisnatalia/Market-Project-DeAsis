e72790a3457896fc842650efabb66c485e10d02a
deasisnatalia <deasisnatalia@gmail.com>
2025-11-25 21:01:23 -0300

    arreglos3

--- mercado/users/adapters.py
+++ mercado/users/adapters.py
@@ -1,28 +1,25 @@
+# users/adapters.py
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.account.utils import user_username
-import unicodedata
-import re
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
-
-        if user.username:
-            return user
-
-        raw = data.get("name") or data.get("email").split("@")[0]
-
-        #Normalize quita acentos, espacios, caracteres
-        raw = unicodedata.normalize('NFKD', raw).encode('ascii', 'ignore').decode('ascii')
-        raw = re.sub(r'[^a-zA-Z0-9_.-]', '', raw).lower()
-
-        if not raw:
-            raw = "user"
-
-        user_username(user, raw) 
+        
+        # Si no existe username, generar uno
+        if not user.username:
+            base = data.get("name") or data.get("email").split("@")[0]
+            username = base.replace(" ", "").lower()
+            
+            # evitar duplicados
+            original = username
+            counter = 1
+            while User.objects.filter(username=username).exists():
+                username = f"{original}{counter}"
+                counter += 1
+            
+            user.username = username
 
         return user
-
-    def is_auto_signup_allowed(self, request, sociallogin):
-        return True
